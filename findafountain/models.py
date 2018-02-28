a359a79ea55e63882b0dccf9e928cea0b5206f83
Giedre Stulgyte <2285161s@student.gla.ac.uk>
2018-02-28 13:07:38 +0000

    html

--- findafountain/models.py
+++ findafountain/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
 	user = models.OneToOneField(User)
-	website = models.URLField(blank = True)
 	picture = models.ImageField(upload_to='profile_images', blank=True)
 
 	def __str__(self):
